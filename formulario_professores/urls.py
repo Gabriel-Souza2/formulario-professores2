972a219dca810d83d274f683d18bb3db1a8b385b
Gabriel de Souza Gomes <gabriel.souza2@hotmail.com.br>
2024-09-14 01:07:34 -0300

    Criando listagem de aulas

--- formulario_professores/urls.py
+++ formulario_professores/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('cadastrar/', views.cadastrar_aula, name='cadastrar_aula'),
     path('sucesso/', views.sucesso, name='sucesso'),  # Uma página simples de sucesso
+    path('aulas/', views.listar_aulas, name='listar_aulas'),
+    path('editar/<int:aula_id>/', views.editar_aula, name='editar_aula'),
+    path('excluir/<int:aula_id>/', views.excluir_aula, name='excluir_aula'),
 ]
--- formulario_professores/views.py
+++ formulario_professores/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AulaForm
+from .models import Aula
 
 
 # Create your views here.
+def listar_aulas(request):
+    aulas = Aula.objects.all()  # Busca todas as aulas cadastradas
+    return render(request, 'listar.html', {'aulas': aulas})
+
 def cadastrar_aula(request):
     if request.method == 'POST':
         form = AulaForm(request.POST)
@@ -13,6 +18,17 @@ def cadastrar_aula(request):
         form = AulaForm()
 
     return render(request, 'formulario.html', {'form': form})
+
+def editar_aula(request, aula_id):
+    aula = get_object_or_404(Aula, id=aula_id)  # Busca a aula pelo ID ou retorna 404
+    if request.method == 'POST':
+        form = AulaForm(request.POST, instance=aula)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_aulas')
+    else:
+        form = AulaForm(instance=aula)
+    return render(request, 'editar_aula.html', {'form': form, 'aula': aula})
 
 def sucesso(request):
     return render(request, 'sucesso.html')
